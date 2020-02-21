8aa7ba37fba4e25d005353ea63e5703c52eaca5c
SaiyemBejoy <jsbejoy96@gmail.com>
2020-02-21 23:48:19 +0600

    Composition

---
+++ Composition/src/com/company/Resoluation.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Resoluation {
+    private int width;
+    private int height;
+
+    public Resoluation(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
